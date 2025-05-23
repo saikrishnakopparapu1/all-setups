202783252ff45937fc0cb6d797bbe60b79b7074f
saikrishnakopparapu1 <saikrishna.kopparapu1@gmail.com>
2025-05-23 11:13:53 +0530

    Update kops.sh
    mykops

--- kops.sh
+++ kops.sh
@@ -16,3 +16,23 @@ aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local 
 export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
 kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
 kops update cluster --name rahams.k8s.local --yes --admin
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+!/bin/bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L-s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
+
+chmod +x kops-linux-amd64 kubectl
+
+mv kubectl /usr/local/bin/kubectl
+
+mv kops-linux-amd64 /usr/local/bin/kops
+
+export KOPS_STATE_STORE=s3://mustafa77.flm.k8s
+
+kops create cluster --name mustafa.k8s.local--zones us-east-1a, us-east-1b--master-count=1-- master-size t2.medium --master-volume-size 30--node-count=2--node-size t2.micro --node- A
+
+volume-size 20
+
+kops update cluster --name mustafa.k8s.local--yes--admin
