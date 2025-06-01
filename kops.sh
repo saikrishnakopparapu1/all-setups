7028872f92171cbe175bc976b78642cfd1703cc1
saikrishnakopparapu1 <saikrishna.kopparapu1@gmail.com>
2025-06-01 18:01:30 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -19,18 +19,12 @@ kops update cluster --name rahams.k8s.local --yes --admin
 
 -----------------------------------------------------------------------------------------------------------------------------
 
-!/bin/bash
-
-curl -LO "https://dl.k8s.io/release/$(curl -L-s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
-
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
-
 mv kubectl /usr/local/bin/kubectl
-
 mv kops-linux-amd64 /usr/local/bin/kops
 
 export KOPS_STATE_STORE=s3://saikrishna.tcs.k8s
-
-kops create cluster --name saikrishna.k8s.local --zones us-east-1a, us-east-1b --master-count 1 --master-size t2.medium --master-volume-size 30 --node-count 2 --node-size t2.micro --node-volume-size 20
-
-kops update cluster --name saikrishna.k8s.local --yes --admin
+kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t3.medium --master-volume-size 30 --node-count=3 --node-size t3.micro --node-volume-size 30
+kops update cluster --name rahams.k8s.local --yes --admin
